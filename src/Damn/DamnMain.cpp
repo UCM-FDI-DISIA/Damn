0f7cfb96309cd59a9135f1ade98544483c704999
DavidRainder <91317502+DavidRainder@users.noreply.github.com>
2024-04-15 14:54:25 +0200

    Funciona igual que CTP

--- src/Damn/DamnMain.cpp
+++ src/Damn/DamnMain.cpp
@@ -1,13 +1,15 @@
 #include "DamnMain.h"
-
 #include "SceneManager.h"
+#include "ComponentFactory.h"
 
-void RegisterComponents()
-{
+#include "Hito2MenuPausa.h"
+#include "Hito2Prueba.h"
 
+void RegisterComponents(eden_ec::ComponentFactory* factory) {
+	factory->RegisterComponent<eden_ec::Hito2MenuPausa>();
+	factory->RegisterComponent<eden_ec::Hito2Prueba>();
 }
 
-void LoadScene(eden::SceneManager* scnManager)
-{
+void LoadScene(eden::SceneManager* scnManager) {
 	scnManager->PushScene("Menu");
 }
--- src/Damn/DamnMain.h
+++ src/Damn/DamnMain.h
@@ -1,12 +1,20 @@
 #ifndef DAMN_MAIN_H_
-
+#define DAMN_MAIN_H_
 #include <string>
 
 namespace eden {
 	class SceneManager;
 }
 
-extern "C" __declspec(dllexport) void RegisterComponents();
-extern "C" __declspec(dllexport) void LoadScene(eden::SceneManager* scnManager);
+namespace eden_ec {
+	class ComponentFactory;
+}
+
+namespace eden_input {
+	class InputManager;
+}
+
+extern "C" __declspec(dllexport) void RegisterComponents(eden_ec::ComponentFactory * factory);
+extern "C" __declspec(dllexport) void LoadScene(eden::SceneManager * scnManager);
 
 #endif // !DAMN_MAIN_H_
