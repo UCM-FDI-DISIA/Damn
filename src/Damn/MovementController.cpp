792fd79b0a8e027c324cc4c15edda587554a94e5
pablosm196 <pablos19@ucm.es>
2024-04-18 17:06:10 +0200

    Componente MovementController

---
+++ src/Damn/MovementController.cpp
@@ -0,0 +1,23 @@
+#include "MovementController.h"
+#include<Entity.h>
+#include<ComponentArguments.h>
+
+void MovementController::Init(eden_script::ComponentArguments* args)
+{
+	_speed = args->GetValueToFloat("Speed");
+}
+
+void MovementController::Start()
+{
+	_rigidBody = _ent->GetComponent<eden_ec::CRigidBody>();
+}
+
+void MovementController::Update(float deltaTime)
+{
+	_rigidBody->SetLinealVelocity(_direction * _speed * deltaTime);
+}
+
+void MovementController::SetDirection(eden_utils::Vector3 newDir)
+{
+	_direction = newDir.Normalized();
+}
---
+++ src/Damn/MovementController.h
@@ -0,0 +1,36 @@
+#ifndef _MOVEMENT_CONTROLLER_H
+#define _MOVEMENT_CONTROLLER_H
+
+#include<Component.h>
+#include<Vector3.h>
+#include<CRigidBody.h>
+
+class MovementController : public eden_ec::Component
+{
+public:
+	MovementController() = default;
+
+	~MovementController() override = default;
+
+	/// @brief Construye el componente dado unos argumentos. Se obtendrán de una lectura de un .lua
+	/// @param args Argumentos leídos de .lua
+	void Init(eden_script::ComponentArguments* args) override;
+
+	/// @brief Metodo heredado de Component que se usa para coger referencias a otros componentes, en este caso el RigidBody de la entidad
+	void Start() override;
+
+	/// @brief Metodo update heredado de Component 
+	/// @param deltaTime El tiempo entre frames calculado por el motor
+	void Update(float deltaTime) override;
+
+	/// @brief Establece la direccion en la que va a avanzar la entidad
+	/// @param newDir Nueva direccion de la entidad
+	void SetDirection(eden_utils::Vector3 newDir);
+private:
+	eden_utils::Vector3 _direction;
+	float _speed;
+	eden_ec::CRigidBody* _rigidBody;
+};
+
+
+#endif
