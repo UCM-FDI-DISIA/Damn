43556f3efe846fa97517ab64bc07963e579e18db
theligero <iligero@ucm.es>
2024-04-23 10:59:05 +0200

    Estructura base componente Health

---
+++ src/Damn/PlayerHealth.cpp
@@ -0,0 +1,2 @@
+#include "PlayerHealth.h"
+
---
+++ src/Damn/PlayerHealth.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_HEALTH_H_
+#define PLAYER_HEALTH_H_
+
+#include "Health.h"
+
+class PlayerHealth : public Health 
+{
+public:
+	PlayerHealth() = default;
+	~PlayerHealth() override = default;
+
+};
+
+#endif // PLAYER_HEALTH_H_
