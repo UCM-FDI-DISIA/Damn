876c75ea06d0e406e0c0b90c5cb0326aeb8d3aaf
DavidRainder <davirive@ucm.es>
2024-04-23 18:55:47 +0200

    Escena de prueba

--- bin/assets/scenes/DamnGame.lua
+++ bin/assets/scenes/DamnGame.lua
@@ -1,31 +1,46 @@
 Entity1 = {
-	Name = "Camera",
+	Name = "Player",
 	Components = {
 		{
 			Name = "TRANSFORM",
 			Arguments = {
-				Position = "-5.921108245849609|-11.02579116821289|1.858309268951416",
-				Rotation = "false|0.646895170211792|0.6883831024169922|-0.23910118639469147|-0.22469086945056915",
+				Position = "0.0|5|10",
+				Rotation = "false|1.0|0.0|0.0|0.0",
 				Scale = "1.0|1.0|1.0"
+			}
+		},
+		{
+			Name = "RIGIDBODY",
+			Arguments = {
+				Mass = "1.0",
+				Bounciness = "0.0",
+				Friction = "1.0",
+				AABB = "1|2|1",
+				PosOffset = "0|0|0",
+				Radius = "0.0",
+				Shape = "BOX",
+				CollisionFlag = "DYNAMIC",
+				CollisionLayer = ""
+
 			}
 		},
 		{
 			Name = "CAMERA",
 			Arguments = {
 			}
+		},
+		{
+			Name = "CAMERA_MOVEMENT",
+			Arguments = { 
+				Sensivity = "5.0"
+			}
 		}
 	}
 }
 
 Entity2 = {
-	Name = "Cube",
+	Name = "Frog",
 	Components = {
-		{
-			Name = "MESH_RENDERER",
-			Arguments = {
-				Mesh = "PruebaCubo"
-			}
-		},
 		{
 			Name = "TRANSFORM",
 			Arguments = {
@@ -37,7 +52,41 @@ Entity2 = {
 	}
 }
 
-Entities = { Entity1, Entity2 }
+Entity3 = {
+	Name = "Floor",
+	Components = {
+		{
+			Name = "TRANSFORM",
+			Arguments = {
+				Position = "0.0|0.0|0.0",
+				Rotation = "true|1.0|0.0|0.0|0.0",
+				Scale = "1|0.01|1"
+			}
+		},
+		{
+			Name = "MESH_RENDERER",
+			Arguments = {
+				Mesh = "cube"
+			}
+		},
+		{
+			Name = "RIGIDBODY",
+			Arguments = {
+				Mass = "1.0",
+				Bounciness = "0.0",
+				Friction = "1.0",
+				AABB = "500|1|500",
+				PosOffset = "0|0|0",
+				Radius = "0.0",
+				Shape = "BOX",
+				CollisionFlag = "STATIC",
+				CollisionLayer = ""
+			}
+		}
+	}
+}
+
+Entities = { Entity1, Entity2, Entity3 }
 
 CollisionLayers = {
 	Layers = {
