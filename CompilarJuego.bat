16674e4391f9762019b0d153e7d173ce94908764
almasso <almasso@ucm.es>
2024-05-10 03:03:52 +0200

    Actualización de la compilación del juego

--- CompilarJuego.bat
+++ CompilarJuego.bat
@@ -3,10 +3,14 @@
 call ActualizarMotor.bat
 
 set GAMEDLLFOLDERS=.\bin
+set sourceFolder=.\EDEN\bin\assets
+set destinationFolder=.\bin\assets
 
 for %%i in (".\EDEN\bin\*") do (
         XCOPY /y "%%i" %GAMEDLLFOLDERS%
 )
+
+xcopy "%sourceFolder%\*" "%destinationFolder%\" /E /Y
 
 msbuild "Damn.sln" /p:configuration=Debug /p:Platform=x64
 msbuild "Damn.sln" /p:configuration=Release /p:Platform=x64
